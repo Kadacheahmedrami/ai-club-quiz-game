717dcb18c12b08bb0eab9cb41ec59ab3b08a7e06
kadacheahmedrami <a_kadache@estin.dz>
2026-02-14 01:08:16 +0100

    adding a small margin in the login comp

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ export default async function LoginPage() {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen mx-2 flex items-center justify-center">
       <div className="bg-gray-800 p-8 rounded-lg shadow-2xl w-full max-w-md">
         <h1 className="text-3xl font-bold text-center text-white mb-8">AI Club Quiz Game</h1>
 
